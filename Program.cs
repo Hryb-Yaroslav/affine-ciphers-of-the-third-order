cee50818d128ffd5e606fa2c7415b71958b8d3d9
Hryb-Yaroslav <90897810+Hryb-Yaroslav@users.noreply.github.com>
2023-05-23 13:12:12 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
      
 class Program
 {
     static public int[,] K1 = {{2,2,3},{2,3,4},{5,3,6}};
     static public int[] K2 = {12,13,14};
-    public static char[] test(string str)
+    public static char[] Code(string str)
     {
         int[] ascc = {(int)str[0] - 65, (int)str[1] - 65, (int)str[2] - 65};
         int[] res1 = new int[3];
@@ -16,14 +16,14 @@ class Program
         }
         return res2; 
     }
-    public static string Code(string text, bool decode = false)
+    public static string CodeDecode(string text, bool decode = false)
     {
         int[,] C = new int[(int)text.Length / 3,3];
         string result = "";
         char[] res;
         for (int i = 0, j = 0; i < C.GetLength(0); i++, j += 3)
         {
-            if (!decode) res = test(text[j] + "" + text[j+1] + "" + text[j+2]);
+            if (!decode) res = Code(text[j] + "" + text[j+1] + "" + text[j+2]);
             else res = Decode(text[j] + "" + text[j+1] + "" + text[j+2]);
             result += "" + res[0] + res[1] + res[2];
         }
@@ -46,8 +46,8 @@ class Program
     public static void Main(String[] args)
     {
         String text = "SHIZZA";
-        string codeText = Code(text);
+        string codeText = CodeDecode(text);
         System.Console.WriteLine(codeText);
-        System.Console.WriteLine(Code(codeText, true));
+        System.Console.WriteLine(CodeDecode(codeText, true));
     }
 }
